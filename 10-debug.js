e6e41f89087c3f453247b9ae021b4a68278e2db0
sajibsaifullah <dusajibphy@gmail.com>
2023-03-16 22:12:58 +0600

    aa

---
+++ 10-debug.js
@@ -0,0 +1,7 @@
+/* 
+1. backward technique ( <----error )
+2. forward technique (event source to forward)
+3. console.log / breakpoint
+4. elimination technique (use comment)
+5. brute force (combination)
+ */
